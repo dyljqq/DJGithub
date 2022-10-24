0fb8ca865df162e1f6ede7946f2a3d544722b522
jiqinqiang <355086587@qq.com>
2022-10-24 19:12:12 +0800

    add reload readme future

--- DJGithub/Github/ViewController/Repo/Views/RepoCell.swift
+++ DJGithub/Github/ViewController/Repo/Views/RepoCell.swift
@@ -8,54 +8,11 @@
 import UIKit
 
 class RepoCell: UITableViewCell {
-
   enum CellType {
     case blank, language(String, String), issues(String), pull, branch(String), readme
-    
-    var height: CGFloat {
-      switch self {
-      case .blank: return 12
-      default: return 44
-      }
-    }
-
-    var imageName: String {
-      switch self {
-      case .language: return "coding"
-      case .issues: return "issue"
-      case .pull: return "pull-request"
-      case .branch: return "git-branch"
-      case .readme: return "book"
-      default: return ""
-      }
-    }
-    
-    var name: String {
-      switch self {
-      case .language(let language, _): return language
-      case .issues: return "Issues"
-      case .pull: return "Pull Requests"
-      case .branch: return "Branches"
-      case .readme: return "README"
-      default: return ""
-      }
-    }
-    
-    var desc: String {
-      switch self {
-      case .language(_, let desc): return desc
-      case .issues(let desc): return desc
-      case .branch(let desc): return desc
-      default: return ""
-      }
-    }
   }
   
-  lazy var backIconLabel: UILabel = {
-    let label = UILabel()
-    
-    return label
-  }()
+  var reloadClosure: (() -> ())?
   
   lazy var iconImageView: UIImageView = {
     let imageView = UIImageView()
@@ -76,6 +33,17 @@ class RepoCell: UITableViewCell {
     return label
   }()
   
+  lazy var reloadImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.image = UIImage(named: "repo_reload")
+    
+    let tap = UITapGestureRecognizer(target: self, action: #selector(reloadAction))
+    imageView.addGestureRecognizer(tap)
+    imageView.isUserInteractionEnabled = true
+    
+    return imageView
+  }()
+  
   override func awakeFromNib() {
     super.awakeFromNib()
   }
@@ -93,13 +61,26 @@ class RepoCell: UITableViewCell {
   func render(with type: CellType) {
     iconImageView.image = UIImage(named: type.imageName)
     nameLabel.text = type.name
-    descLabel.text = type.desc
+    
+    if type.showReload {
+      descLabel.isHidden = true
+      reloadImageView.isHidden = false
+    } else {
+      descLabel.isHidden = false
+      reloadImageView.isHidden = true
+      descLabel.text = type.desc
+    }
+  }
+  
+  @objc func reloadAction() {
+    reloadClosure?()
   }
   
   private func setUp() {
     contentView.addSubview(iconImageView)
     contentView.addSubview(nameLabel)
     contentView.addSubview(descLabel)
+    contentView.addSubview(reloadImageView)
     
     iconImageView.snp.makeConstraints { make in
       make.leading.equalTo(12)
@@ -114,6 +95,58 @@ class RepoCell: UITableViewCell {
       make.trailing.equalTo(-12)
       make.centerY.equalTo(contentView)
     }
+    reloadImageView.snp.makeConstraints { make in
+      make.trailing.equalTo(-16)
+      make.centerY.equalTo(contentView)
+      make.width.height.equalTo(20)
+    }
   }
 
+}
+
+extension RepoCell.CellType {
+  var height: CGFloat {
+    switch self {
+    case .blank: return 12
+    default: return 44
+    }
+  }
+
+  var imageName: String {
+    switch self {
+    case .language: return "coding"
+    case .issues: return "issue"
+    case .pull: return "pull-request"
+    case .branch: return "git-branch"
+    case .readme: return "book"
+    default: return ""
+    }
+  }
+  
+  var name: String {
+    switch self {
+    case .language(let language, _): return language
+    case .issues: return "Issues"
+    case .pull: return "Pull Requests"
+    case .branch: return "Branches"
+    case .readme: return "README"
+    default: return ""
+    }
+  }
+  
+  var desc: String {
+    switch self {
+    case .language(_, let desc): return desc
+    case .issues(let desc): return desc
+    case .branch(let desc): return desc
+    default: return ""
+    }
+  }
+
+  var showReload: Bool {
+    switch self {
+    case .readme: return true
+    default: return false
+    }
+  }
 }
