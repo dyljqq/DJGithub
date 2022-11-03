0f44b9a9a85aa9d3ccff4c651e10c336d2159598
jiqinqiang <355086587@qq.com>
2022-11-03 22:48:40 +0800

    add fps monitor

---
+++ DJGithub/DJMonitor/DJFPS/DJFPSLabel.swift
@@ -0,0 +1,20 @@
+//
+//  DJFPSLabel.swift
+//  DJGithub
+//
+//  Created by jiqinqiang on 2022/11/3.
+//
+
+import UIKit
+
+class DJFPSLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
