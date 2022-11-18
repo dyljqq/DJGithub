f84cd0d16706fe424edab12a3b9de96511fb21b0
jiqinqiang <355086587@qq.com>
2022-11-18 16:16:41 +0800

    架构适配

--- DJGithub/DJMonitor/mach_stack_info.c
+++ DJGithub/DJMonitor/mach_stack_info.c
@@ -10,6 +10,38 @@
 #include <stdlib.h>
 #include <machine/_mcontext.h>
 
+#if defined __i386__
+#define THREAD_STATE_FLAVOR x86_THREAD_STATE
+#define THREAD_STATE_COUNT  x86_THREAD_STATE_COUNT
+#define __framePointer      __ebp
+#define __programCounter    __eip
+#define __stackPointer      __esp
+
+#elif defined __x86_64__
+#define THREAD_STATE_FLAVOR x86_THREAD_STATE64
+#define THREAD_STATE_COUNT  x86_THREAD_STATE64_COUNT
+#define __framePointer      __rbp
+#define __programCounter    __rip
+#define __stackPointer      __rsp
+
+#elif defined __arm__
+#define THREAD_STATE_FLAVOR ARM_THREAD_STATE
+#define THREAD_STATE_COUNT  ARM_THREAD_STATE_COUNT
+#define __framePointer      __r[7]
+#define __programCounter    __pc
+#define __stackPointer      __sp
+
+#elif defined __arm64__
+#define THREAD_STATE_FLAVOR ARM_THREAD_STATE64
+#define THREAD_STATE_COUNT  ARM_THREAD_STATE64_COUNT
+#define __framePointer      __fp
+#define __programCounter    __pc
+#define __stackPointer      __sp
+
+#else
+#error "Current CPU Architecture is not supported"
+#endif
+
 /**
  
  Stack frame structure for arm/arm64:
@@ -40,10 +72,10 @@
  */
 int mach_frame_count(thread_t thread, void **stack, int maxSymbolCount) {
   _STRUCT_MCONTEXT machineContext;
-  mach_msg_type_number_t stateCount = ARM_THREAD_STATE64_COUNT;
+  mach_msg_type_number_t stateCount = THREAD_STATE_COUNT;
   
   kern_return_t kerr = thread_get_state(thread,
-                                        ARM_THREAD_STATE64,
+                                        THREAD_STATE_FLAVOR,
                                         (thread_state_t)&(machineContext.__ss),
                                         &stateCount);
   if (kerr != KERN_SUCCESS) {
@@ -51,10 +83,11 @@ int mach_frame_count(thread_t thread, void **stack, int maxSymbolCount) {
   }
   
   int i = 0;
-  stack[i++] = (void *)machineContext.__ss.__pc;
-  stack[i++] = (void *)machineContext.__ss.__lr;
-
-  void **currentFramePointer = (void **)machineContext.__ss.__fp;
+  stack[i++] = (void *)machineContext.__ss.__programCounter;
+#if defined(__arm__) || defined (__arm64__)
+    stack[i++] = (void *)machineContext.__ss.__lr;
+#endif
+  void **currentFramePointer = (void **)machineContext.__ss.__framePointer;
   while (i < maxSymbolCount && !currentFramePointer) {
     void **previous = *currentFramePointer;
     if (!previous) break;
